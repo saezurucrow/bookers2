0331a96983d14182d721758ce8c1e927cbc3442c
saezurucrow <usobukuhito@gmail.com>
2019-11-02 04:35:31 +0000

    model作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
 
-    has_many :books
+    has_many :books, dependent: :destroy
+    has_many :post_comments, dependent: :destroy
 
     attachment :profile_image
 
