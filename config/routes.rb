abbb4794b7fa79a7301a001395fb194d0bf306ed
saezurucrow <usobukuhito@gmail.com>
2019-12-03 01:34:47 +0000

    フォロー機能始め

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   devise_for :users
   root 'homes#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_023947) do
+ActiveRecord::Schema.define(version: 2019_12_03_013316) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2019_11_07_023947) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
