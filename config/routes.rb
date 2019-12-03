fad4ac37777de869916f117325c388cf83ddbbc7
saezurucrow <usobukuhito@gmail.com>
2019-12-03 01:37:42 +0000

    routing,association設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
   end
 
-  resources :users, only: [:new, :index, :show, :edit, :update]
+  resources :users, only: [:new, :index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member # 追加
+    get :followers, on: :member # 追加
+  end
   resources :homes, only: [:index]
   get "home/about" => "homes#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
